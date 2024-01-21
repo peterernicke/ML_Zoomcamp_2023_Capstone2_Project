e402957dfde6ed7cc640ac68c690ea5a26f89004
Peter Ernicke <pernicke@gmail.com>
2024-01-21 15:02:12 +0100

    add deployment

---
+++ Deployment/la_fu.py
@@ -0,0 +1,78 @@
+import os
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from tensorflow import keras
+from tempfile import NamedTemporaryFile
+
+# Function to load and preprocess a single example
+def preprocess_example(img):
+    input_image = np.array(Image.open(img)) / 255.0
+    return np.expand_dims(input_image, axis=0)
+
+def recolor_prediction(predicted_mask):
+    # Define colors for each class
+    class_colors = {
+        0: (0, 0, 0),        # unlabeled
+        1: (255, 0, 0),      # buildings
+        2: (34, 139, 34),    # woodlands
+        3: (0, 0, 255),      # water
+        4: (184, 115, 51)    # road
+    }
+
+    # Convert ground truth image to numpy array
+    pred_array = np.array(predicted_mask)
+
+    # Apply color mapping
+    pred_colored = np.zeros(pred_array.shape + (3,), dtype=np.uint8)
+    for class_label, color in class_colors.items():
+        pred_colored[pred_array == class_label] = np.array(color)
+    
+    return pred_colored
+
+def lambda_handler(event, context):
+    try:
+        # Extract the image content from the Lambda event
+        image_content = event['body']
+        
+        # Save the image content to a temporary file
+        with NamedTemporaryFile(suffix=".tif", delete=False) as temp_file:
+            temp_file.write(image_content)
+            temp_file_path = temp_file.name
+
+        # Load the model
+        model_path = "final-model.keras"  # Update with your model path
+        model = keras.models.load_model(model_path)
+
+        # Load and preprocess input image
+        input_image = preprocess_example(temp_file_path)
+
+        # Predict with the model
+        predictions = model.predict(input_image)
+        predicted_mask = np.argmax(predictions[0], axis=-1)
+
+        colored_predicted_mask = recolor_prediction(predicted_mask)
+
+        PIL_image = Image.fromarray(colored_predicted_mask.astype('uint8'), 'RGB')
+
+        # Save the image to a BytesIO object
+        image_bytes = BytesIO()
+        PIL_image.save(image_bytes, format='PNG')
+        image_bytes.seek(0)
+
+        # Read the BytesIO object content and convert to bytes
+        image_content_bytes = image_bytes.read()
+
+        return {
+            'statusCode': 200,
+            'body': image_content_bytes,
+            'isBase64Encoded': False,
+            'headers': {
+                'Content-Type': 'image/png'
+            }
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
---
+++ Deployment/test.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from PIL import Image
+from io import BytesIO
+
+url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+
+# Load the TIFF image from file or any other source
+image_path = "./../Data50_res256_filtered_20/validate/sat/sat_N-33-60-D-d-1-2_02_01.tif"
+with open(image_path, 'rb') as file:
+    image_data = file.read()
+
+# Create a JSON payload with the image data
+payload = {
+    'body': image_data.decode('latin-1')  # Convert binary data to string
+}
+
+# Send the POST request to your Lambda endpoint
+response = requests.post(url, data=json.dumps(payload))
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Convert the response content (image bytes) to a PIL Image and save it
+    predicted_image = Image.open(BytesIO(response.content))
+    
+    # Save the predicted image locally
+    predicted_image.save("./predicted_image.png")
+    print("Prediction successful. Image saved.")
+else:
+    print("Error:", response.text)
